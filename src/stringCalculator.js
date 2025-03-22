0268f055727202e1c5ce646aa59e2003b2787a6d
Sushan <skmac@Sushans-MacBook-Pro.local>
2025-03-22 19:44:03 +0545

    Feat: Support newline as delimiter (Passing)

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,7 @@
 class StringCalculator {
     add(numbers) {
         if (numbers === "") return 0;
-        return numbers.split(',').map(Number).reduce((sum, num) => sum + num, 0);
+        return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
     }
 }
 
