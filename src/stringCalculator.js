5b7db1141d7f34e40934e58c73bd5977a950fcf6
Sushan <skmac@Sushans-MacBook-Pro.local>
2025-03-22 20:55:26 +0545

    Feat: Support for  Custom delimiters (Passing)

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,14 @@
 class StringCalculator {
     add(numbers) {
         if (numbers === "") return 0;
+
+        if (numbers.startsWith("//")) {
+            const delimiterLineEnd = numbers.indexOf("\n");
+            const delimiter = numbers.slice(2, delimiterLineEnd);
+            numbers = numbers.slice(delimiterLineEnd + 1);
+            return numbers.split(new RegExp(`\\${delimiter}`)).map(Number).reduce((sum, num) => sum + num, 0);
+        }
+
         return numbers.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
     }
 }
