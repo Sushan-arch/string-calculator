20691d42fbef0f517142b3ead8f938832166db0c
Sushan <skmac@Sushans-MacBook-Pro.local>
2025-03-22 19:36:06 +0545

    Test: Return single number input (Failing)

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -10,4 +10,8 @@ describe('StringCalculator', () => {
     test('returns 0 for an empty string', () => {
         expect(calculator.add("")).toBe(0);
     });
+
+    test('returns the number itself when one number is provided', () => {
+        expect(calculator.add("5")).toBe(5);
+    });
 });
